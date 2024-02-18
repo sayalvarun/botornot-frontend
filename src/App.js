bab38ae77d778ba807b96a3e068378be8ed6103a
root <root@Olympus.localdomain>
2024-02-17 21:28:24 -0500

    Make the logo and the color change on click

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { useState } from 'react';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRobot, faUser } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   const [selectedIcons, setSelectedIcons] = useState(Array(5).fill(null));
+  const [iconColors, setIconColors] = useState(Array(5).fill('#000'));
 
   const handleIconClick = (cardIndex, icon) => {
     setSelectedIcons(prevIcons => {
       const newIcons = [...prevIcons];
       newIcons[cardIndex] = icon;
       return newIcons;
+    });
+
+    setIconColors(prevColors => {
+      const newColors = [...prevColors];
+      newColors[cardIndex] = icon === 'robot' ? '#3B82F6' : '#c30000';
+      return newColors;
     });
   };
 
@@ -17,24 +26,18 @@ function App() {
       <div className="prompt-text">Enter your prompt here</div>
       <div className="cards-container">
         {[...Array(5)].map((_, i) => (
-          <div key={i} className={`card ${selectedIcons[i] === 'robot' ? 'red' : selectedIcons[i] === 'human' ? 'green' : ''}`}>
+          <div key={i} className={`card ${selectedIcons[i] === 'robot' ? 'robot' : selectedIcons[i] === 'human' ? 'human' : ''}`}>
             
-            <span 
-                className="icon-robot"
-                onClick={() => handleIconClick(i, 'robot')}
-              >
-                🤖
-              </span>
+            <div className="icon-robot"  onClick={() => handleIconClick(i, 'robot')}>
+              <FontAwesomeIcon icon={faRobot} color={selectedIcons[i] === 'robot' ? iconColors[i] : '#000'} />
+            </div>
             <div className="card-content">
               <p>Your text goes here and can be very long up to 140 characters...</p>
             </div>
             
-            <span 
-                className="icon-human"
-                onClick={() => handleIconClick(i, 'human')}
-              >
-                👤
-              </span>
+            <div className="icon-human"  onClick={() => handleIconClick(i, 'human')}>
+              <FontAwesomeIcon icon={faUser} color={selectedIcons[i] === 'human' ? iconColors[i] : '#000'}/>
+            </div>
           </div>
         ))}
       </div>
